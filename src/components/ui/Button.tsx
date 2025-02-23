9a923364257f66cf0c7c2edbc12419ab92bb77ce
Sathyapriyan Chinnathangam <42141034+nsathya1990@users.noreply.github.com>
2025-02-23 20:51:38 +0000

    Update Button.tsx

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,4 +1,4 @@
-const Button = ({ children, className = "" }) => {
+const Button = ({ children, className = "" }: { children: any; className?: string }) => {
   return (
     <button
       className={`bg-green-600 hover:bg-green-700 text-white px-6 py-3 rounded-full shadow-md ${className}`}
